bd4eed12eb1b4986882de3a158a57d21cd65ddba
eodevelop <eodevelop@naver.com>
2024-07-26 20:31:11 +0900

    first commit

---
+++ src/main/kotlin/com/minjae/kotlin_laboratory/KotlinLaboratoryApplication.kt
@@ -0,0 +1,11 @@
+package com.minjae.kotlin_laboratory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinLaboratoryApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinLaboratoryApplication>(*args)
+}
---
+++ src/test/kotlin/com/minjae/kotlin_laboratory/KotlinLaboratoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.minjae.kotlin_laboratory
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinLaboratoryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
